e25477b103da315c1812956926d43f98519f1547
Aziz <azizplays895586@gmail.com>
2026-05-26 15:09:38 +0530

    changes in js file

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const http = require('http');
+const fs = require("fs");
 const server = http.createServer((req,res)=>{
+    fs.readFile("index.html", (err, data) => {
+    res.write(data);
+    res.end();
+  });
   console.log(req)
   res.setHeader("Content-Type","text/html")
   const url=req.url
   if(url==="/"){
     res.write(`<h1>i learned about installing npm and nodemon</h1>`)
-  }
+  }res.end()
 
 })
 
