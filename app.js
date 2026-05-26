8dc62c023418f76240940ec67f892c4c1aa49cd6
Aziz <azizplays895586@gmail.com>
2026-05-26 15:12:50 +0530

    updated changes

--- app.js
+++ app.js
@@ -1,22 +1,26 @@
 const http = require('http');
 const fs = require("fs");
-const server = http.createServer((req,res)=>{
-    fs.readFile("index.html", (err, data) => {
+
+const server = http.createServer((req, res) => {
+
+  res.setHeader("Content-Type", "text/html");
+
+  fs.readFile("index.html", (err, data) => {
+
+    if (err) {
+      res.write("File not found");
+      res.end();
+      return;
+    }
+
     res.write(data);
     res.end();
   });
-  console.log(req)
-  res.setHeader("Content-Type","text/html")
-  const url=req.url
-  if(url==="/"){
-    res.write(`<h1>i learned about installing npm and nodemon</h1>`)
-  }res.end()
 
-})
+});
 
+let Port = 6003;
 
-let Port=6003;
-server.listen(Port,()=>{
- console.log(` sever running on http://localhost:${Port}`)
-})
-
+server.listen(Port, () => {
+  console.log(`server running on http://localhost:${Port}`);
+});
